4f4f031d73fa2a278b05cea18c568ac66434df53
Daniel Cestari <dcestari@gmail.com>
2011-08-10 22:11:00 -0430

    Migrate code to latest Symfony2 with git submodules

--- app/autoload.php
+++ app/autoload.php
@@ -5,16 +5,16 @@ use Symfony\Component\ClassLoader\UniversalClassLoader;
 $loader = new UniversalClassLoader();
 $loader->registerNamespaces(array(
     'Symfony'          => array(__DIR__.'/../vendor/symfony/src', __DIR__.'/../vendor/bundles'),
-    'Doctrine\\Common' => __DIR__.'/../vendor/symfony/vendor/doctrine-common/lib',
-    'Doctrine\\DBAL'   => __DIR__.'/../vendor/symfony/vendor/doctrine-dbal/lib',
-    'Doctrine'         => __DIR__.'/../vendor/symfony/vendor/doctrine/lib',
-    'Monolog'          => __DIR__.'/../vendor/symfony/vendor/monolog/src',
+    'Doctrine\\Common' => __DIR__.'/../vendor/doctrine-common/lib',
+    'Doctrine\\DBAL'   => __DIR__.'/../vendor/doctrine-dbal/lib',
+    'Doctrine'         => __DIR__.'/../vendor/doctrine/lib',
+    'Monolog'          => __DIR__.'/../vendor/monolog/src',
     'Assetic'          => __DIR__.'/../vendor/assetic/src',
-    'Metadata'         => __DIR__.'/../vendor/symfony/vendor/metadata/src',
+    'Metadata'         => __DIR__.'/../vendor/metadata/src',
 ));
 $loader->registerPrefixes(array(
-    'Twig_Extensions_' => __DIR__.'/../vendor/symfony/vendor/twig-extensions/lib',
-    'Twig_'            => __DIR__.'/../vendor/symfony/vendor/twig/lib',
+    'Twig_Extensions_' => __DIR__.'/../vendor/twig-extensions/lib',
+    'Twig_'            => __DIR__.'/../vendor/twig/lib',
 ));
 $loader->registerPrefixFallbacks(array(
     __DIR__.'/../vendor/symfony/src/Symfony/Component/Locale/Resources/stubs',
@@ -26,5 +26,5 @@ $loader->register();
 
 // Swiftmailer needs a special autoloader to allow
 // the lazy loading of the init file (which is expensive)
-require_once __DIR__.'/../vendor/symfony/vendor/swiftmailer/lib/classes/Swift.php';
-Swift::registerAutoload(__DIR__.'/../vendor/symfony/vendor/swiftmailer/lib/swift_init.php');
+require_once __DIR__.'/../vendor/swiftmailer/lib/classes/Swift.php';
+Swift::registerAutoload(__DIR__.'/../vendor/swiftmailer/lib/swift_init.php');
